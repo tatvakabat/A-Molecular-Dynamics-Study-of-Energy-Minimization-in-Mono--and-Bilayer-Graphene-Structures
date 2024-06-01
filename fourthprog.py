e1da1b07941b430cfc600362e152862c625b7eff
tatvakabat <145942262+tatvakabat@users.noreply.github.com>
2024-05-31 22:11:44 -0400

    Add files via upload

---
+++ fourthprog.py
@@ -0,0 +1,3 @@
+hours = input("Number of Hours: ")
+rate = input("Rate Per Hour: ")
+print("Gross Pay: ", float(hours)*float(rate))
---
+++ thirdprog.py
@@ -0,0 +1,2 @@
+inp = input("What is your name: ")
+print("Hello " + inp)
